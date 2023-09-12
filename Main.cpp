f0d05988c3f70914b2aac81e955040c8480e35df
Networt19 <144666401+Networt19@users.noreply.github.com>
2023-09-12 07:42:07 +0000

    Sobrecarga.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,3 +1,16 @@
-int main(){
-    return 0; 
+#include<iostream>
+using namespace std;
+
+
+auto sum(int a){
+    return [a](int b){return a+b}
+}
+
+
+int main()
+{
+    auto result =sum(5);
+    int a = result(100);
+    int b = result(30);
+    cout << a << b <<end1;
 }
